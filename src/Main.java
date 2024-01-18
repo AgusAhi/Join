26ccc3ea79881e82726b3f488309f9df3b6a8f0b
aad <aguahidol@alu.edu.gva.es>
2024-01-18 19:48:38 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        Thread thread = new Thread(new MessageLog());
+    public static void main(String[] args) throws InterruptedException {
+        int seconds = 0;
+        if (args.length > 0) {
+            seconds = Integer.parseInt(args[0]);
+        }
+        String[] messages = {
+            "Message 1", "Message 2", "Message 3", "Message 4"};
+        MessageLog msg = new MessageLog(messages);
+        Thread thread = new Thread(msg);
         thread.start();
-        for ()
+        int count = 0;
+        while (count < seconds) {
+            Thread.sleep(1000);
+            count++;
+            if (thread.isAlive() && count >= seconds) {
+                thread.interrupt();
+            }
+        }
+        thread.join();
+        System.out.println("The End of Evangelion");
     }
 }
